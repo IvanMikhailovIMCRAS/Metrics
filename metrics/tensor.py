e96ee099bf60ffa42e2dbb816de1c0b625dce4cd
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-04-03 15:39:57 +0300

    Add Tensor class

--- metrics/tensor.py
+++ metrics/tensor.py
@@ -1,10 +1,9 @@
+from typing import Tuple
 import numpy as np
-from typing import Tuple 
-import matplotlib.pyplot as plt
-from periodic_box import *
+from periodic_box import Box
 
 
-def mass_center(x:np.ndarray, y:np.ndarray, z:np.ndarray) -> Tuple[float, float, float]:
+def mass_center(x: np.ndarray, y: np.ndarray, z: np.ndarray) -> Tuple[float, float, float]:
     if not (len(x) == len(y) == len(z)):
         raise ValueError(" Different length of arrays")
     x_m = np.mean(x)
@@ -12,14 +11,15 @@ def mass_center(x:np.ndarray, y:np.ndarray, z:np.ndarray) -> Tuple[float, float,
     z_m = np.mean(z)
     return x_m, y_m, z_m
 
-def gyration_tensor(x:np.ndarray, y:np.ndarray, z:np.ndarray) -> np.ndarray:
+
+def gyration_tensor(x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
     if not (len(x) == len(y) == len(z)):
         raise ValueError(" Different length of arrays")
-    x_m, y_m, z_m = mass_center(x, y, z)  
-    x -= x_m  
-    y -= y_m 
-    z -= z_m 
-    T = np.zeros(shape=(3,3), dtype=float)
+    x_m, y_m, z_m = mass_center(x, y, z)
+    x -= x_m
+    y -= y_m
+    z -= z_m
+    T = np.zeros(shape=(3, 3), dtype=float)
     for i in range(len(x)):
         T[0][0] += x[i]*x[i]
         T[0][1] += x[i]*y[i]
@@ -32,10 +32,11 @@ def gyration_tensor(x:np.ndarray, y:np.ndarray, z:np.ndarray) -> np.ndarray:
     T[2][1] = T[1][2]
     return T/len(x)
 
-def full_gyration_tensor(x:np.ndarray, y:np.ndarray, z:np.ndarray, box: Box) -> np.ndarray:
+
+def full_gyration_tensor(x: np.ndarray, y: np.ndarray, z: np.ndarray, box: Box) -> np.ndarray:
     if not (len(x) == len(y) == len(z)):
         raise ValueError(" Different length of arrays")
-    T = np.zeros(shape=(3,3), dtype=float)
+    T = np.zeros(shape=(3, 3), dtype=float)
     for i in range(len(x)):
         for j in range(len(x)):
             dx = x[i] - x[j]
@@ -52,53 +53,81 @@ def full_gyration_tensor(x:np.ndarray, y:np.ndarray, z:np.ndarray, box: Box) -> 
     T[2][0] = T[0][2]
     T[2][1] = T[1][2]
     return T/(2*len(x)**2)
-    
-def gyration_radius(x:np.ndarray, y:np.ndarray, z:np.ndarray) -> float:
-    if not (len(x) == len(y) == len(z)):
-        raise ValueError(" Different length of arrays")
-    x_m, y_m, z_m = mass_center(x, y, z)  
-    x -= x_m  
-    y -= y_m 
-    z -= z_m 
-    return (np.mean(np.sum(x**2)) + np.mean(np.sum(y**2)) + np.mean(np.sum(z**2)))/len(x)
+
+
+
+
+class Tensor():
+    def __init__(self, T):
+        self.T = T
+        self.Rx, self.Ry, self.Rz = sorted(np.linalg.eigvals(self.T))
+
+    def __str__(self):
+        return f"Rx = {self.Rx}, Ry = {self.Ry}, Rz = {self.Rz}"
+
+    @property
+    def principal_moments(self):
+        return self.Rx, self.Ry, self.Rz
+
+    @property
+    def gyration_radius(self):
+        return np.sqrt(self.Rx + self.Ry + self.Rz)
+
+    @property
+    def momets_ratio(self):
+        return self.Rx / self.Rz, self.Ry / self.Rz, self.Ry / self.Rx
+
+    @property
+    def asphericity(self):
+        return self.Rz - 0.5 * (self.Rx + self.Ry)
+
+    @property
+    def acylindricity(self):
+        return self.Ry - self.Rx
+
+    @property
+    def shape_anisotropy(self):
+        return 1.5 * (self.Rx**2 + self.Ry**2 + self.Rz**2)/((self.Rx + self.Ry + self.Rz)**2) - 0.5
+
 
 if __name__ == "__main__":
     np.random.seed(42)
-    box_size = 60
-    box = Box(box_size, box_size, box_size)
-    r = 3 # radius of sphere
-    length = 0
-    center = (-3, -3, -3)
-    x, y, z = [], [], []
-    while length < 10:
-        x_t, y_t, z_t  = np.random.uniform(-r, r, 3)
-        if (x_t**2 + y_t**2 + z_t**2 < r**2):
-            x_t = x_t + center[0]
-            y_t = y_t + center[1]
-            z_t = z_t + center[2]
-            # x_t, y_t, z_t = box.periodic_correct(x_t, y_t, z_t)
-            length += 1
-            x.append(x_t)
-            y.append(y_t)
-            z.append(z_t)
-    x = np.array(x)
-    y = np.array(y)
-    z = np.array(z)
-    m_center = mass_center(x, y, z)
-    g_tensor = gyration_tensor(x, y, z)
-    full_g_tensor = full_gyration_tensor(x, y, z, box)
-    print(f"m_center = {m_center}")
-    # print(f"g_tensor = {g_tensor}")
-    # print(f"python_metho{np.linalg.eigvals(g_tensor)}")
-    # print(gyration_radius(x, y, z))
-    print(np.sum(np.linalg.eigvals(g_tensor)))
-    print(np.sum(np.linalg.eigvals(full_g_tensor)))
-    
-    # plt.scatter(x, y)
-    # plt.show()
-              
-        
-        
-        
-    
-    
+    # box_size = 60
+    # box = Box(box_size, box_size, box_size)
+    # r = 3  # radius of sphere
+    # length = 0
+    # center = (-3, -3, -3)
+    # x, y, z = [], [], []
+    # while length < 500:
+    #     x_t, y_t, z_t = np.random.uniform(-r, r, 3)
+    #     y_t = 0.
+    #     z_t = 0.
+    #     if (x_t**2 + y_t**2 + z_t**2 < r**2):
+    #         x_t = x_t + center[0]
+    #         y_t = y_t + center[1]
+    #         z_t = z_t + center[2]
+    #         # x_t, y_t, z_t = box.periodic_correct(x_t, y_t, z_t)
+    #         length += 1
+    #         x.append(x_t)
+    #         y.append(y_t)
+    #         z.append(z_t)
+    # x = np.array(x)
+    # y = np.array(y)
+    # z = np.array(z)
+    # m_center = mass_center(x, y, z)
+    # g_tensor = gyration_tensor(x, y, z)
+    # full_g_tensor = full_gyration_tensor(x, y, z, box)
+    # # print(f"m_center = {m_center}")
+    # # print(f"g_tensor = {g_tensor}")
+    # # print(f"python_metho{np.linalg.eigvals(g_tensor)}")
+    # # print(gyration_radius(x, y, z))
+    # # print(np.sum(np.linalg.eigvals(g_tensor)))
+    # # print(np.sum(np.linalg.eigvals(full_g_tensor)))
+    # tensor = Tensor(g_tensor)
+    # print(tensor)
+    # print(tensor.acylindricity)
+    # print(tensor.asphericity)
+    # print(tensor.gyration_radius)
+    # print(tensor.shape_anisotropy)
+    # # plt.scatter(x, y)
+    # # plt.show()
